672cdae3c1d71d6e9fdb61548a391da00a845aa6
DANeoOfSympnoia <daniel.peters.014@gmail.com>
2026-07-09 12:58:12 +0200

    Update update_players_matchhistory.py
    added pseudocode

--- scripts/update_players_matchhistory.py
+++ scripts/update_players_matchhistory.py
@@ -1,1 +1,26 @@
 # PLACEHOLDER - this will update palyers.csv and matchhistory.csv
+
+#pseudocode: 
+# import contents of matches folder
+# read names in team_a, team_b of each row of each file, pass cleannames (lower case, connected by "_") of names 
+# for each cleanname check in players.csv:
+# does cleanname match an existing cleanname assigned to a player id? 
+# if yes: do nothing
+# if no:
+#   
+#   does cleanname match to a starting part of a cleanname assigned to a player id? 
+#     --> promppt (example): Do you want to add "mo" as an identifier for mo_re, [moritz_regenstein]? yes --> add mo to the list of cleannames for mo_re, no --> print "invalid entry, please update the names of match_id"
+#   does cleanname match to a starting part of multiple cleanname assigned to a player_id's? --> print "invalid entry, please update the names of match_id" (example: Kons matches to ko_st, [konstantin_steuer] and ko_ex, [konstantin_example]
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
