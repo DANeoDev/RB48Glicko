9ac74acd3f53ac81f791950a296dc3997eac799b
DANeo <daniel.peters.014@gmail.com>
2026-08-11 16:13:15 +0200

    added calibration ratings and player statistics

---
+++ TO-DOs.md
@@ -0,0 +1,8 @@
+Random List of to-dos for later:
+
+- Test different outcomes for different starting sigma and RD values
+
+- Add a feature that let's you calculate the ratings only for specific timeframes
+- Seperate ratings and stats for HF and Box
+
+- Create rating-seed when creating new player (use calibrations to do it manually as of now)
--- scripts/update_players.py
+++ scripts/update_players.py
@@ -1,71 +1,15 @@
 from pathlib import Path
 import csv
-
+from loaders import (
+    load_players,
+    create_alias_lookup,
+    get_ignored_aliases,
+    read_names_from_matchhistory
+)
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
-MATCHHISORY_FILE = PROJECT_ROOT / "data" / "matchhistory.csv"
 IGNORED_ALIASES_FILE = PROJECT_ROOT / "data" / "ignored_aliases.csv"
 PLAYERS_FILE = PROJECT_ROOT / "data" / "players.csv"
 
-
-def get_existing_player_data():
-    players_dict = {}
-    
-
-    if PLAYERS_FILE.exists():
-        with open(PLAYERS_FILE, "r", newline="", encoding="utf-8") as h_file:
-            reader = csv.DictReader(h_file)
-            for row in reader:                
-                if not row: 
-                    continue
-                player_id = int(row["player_id"])
-                aliases = row["aliases"].split(";")
-                positions = row["positions"].split(";")
-
-                players_dict[player_id]={
-                    "aliases":aliases,
-                    "positions":positions
-                }
-
-    return players_dict
-
-
-def create_alias_lookup(players_dict):
-    
-    alias_lookup_dict = {
-        alias: player_id
-        for player_id, data in players_dict.items()
-        for alias in data["aliases"]    
-    }
-    return alias_lookup_dict
-
-def get_ignored_aliases():
-    ignored_aliases = set()
-
-    if IGNORED_ALIASES_FILE.exists():
-        with open(IGNORED_ALIASES_FILE, "r", newline="", encoding="utf-8") as h_file:
-            reader = csv.DictReader(h_file)
-
-            for row in reader:
-                ignored_aliases.add(row["alias"])
-
-    return ignored_aliases
-  
-def read_names_from_matchhistory():
-    if MATCHHISORY_FILE.exists():
-        with open(MATCHHISORY_FILE, "r", newline="", encoding="utf-8") as h_file:
-            reader=csv.DictReader(h_file)
-            all_names=[]
-            for row in reader:
-                if not row:
-                    continue
-                names = [name.strip() for name in row["team_a"].split(",") + row["team_b"].split(",")]
-
-                if row["match_id"].startswith("#"):
-                    continue
-                
-                all_names.extend(names)
-
-    return list(dict.fromkeys(all_names))
 
 def print_players(players_dict):
     print("\nExisting players:")
@@ -154,7 +98,6 @@ def new_player_prompt(players_dict, alias_lookup_dict, name_inputs, ignored_alia
 
         write_players(players_dict)
 
-    
 
 def write_players(players_dict):
     with open(PLAYERS_FILE, "w", newline="", encoding="utf-8") as h_file:
@@ -171,6 +114,7 @@ def write_players(players_dict):
                 "aliases": ";".join(data["aliases"]),
                 "positions": ";".join(data["positions"])
             })
+
 
 def write_ignored_aliases(ignored_aliases):
     with open(
@@ -192,16 +136,15 @@ def write_ignored_aliases(ignored_aliases):
                 "alias": alias
             })
 
-
             
 
 def update_players():
-    players_dict = get_existing_player_data()
-    alias_lookup_dict = create_alias_lookup(players_dict)
+    players = load_players()
+    alias_lookup_dict = create_alias_lookup(players)
     name_inputs = read_names_from_matchhistory()
     ignored_aliases = get_ignored_aliases()
 
-    new_player_prompt(players_dict, alias_lookup_dict, name_inputs, ignored_aliases)
+    new_player_prompt(players, alias_lookup_dict, name_inputs, ignored_aliases)
 
     
    
